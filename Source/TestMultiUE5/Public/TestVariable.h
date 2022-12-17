f1045dfd22478d266e2f5b3ed13fdbfdd58776ea
Hugo Forel <forel.hugo@gmail.com>
2022-12-17 22:27:27 +0100

    last push de la préprod

---
+++ Source/TestMultiUE5/Public/TestVariable.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TESTMULTIUE5_API TestVariable
+{
+public:
+
+	UPROPERTY(BluePrintReadWrite)
+		int intTest = 2;
+
+
+	TestVariable();
+	~TestVariable();
+};
